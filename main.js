2ca3613061007aa4db3d41a3bade5b520263102a
Emmanuel Johnson <emmanueljohnso1998@gmail.com>
2026-02-26 13:14:31 -0500

    debugged and added styles

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 │        UI Layer            │
 │  - Input: total            │
 │  - Input: tendered         │
-│  - Button: Calculate       │
+│  - Button: Calculate       │DONE
 │  - Output Display Area     │
 └──────────────┬─────────────┘
                │ click event
@@ -12,7 +12,7 @@
 ┌────────────────────────────┐
 │   Event / Control Layer    │
 │  - Reads input values      │
-│  - Validates               │
+│  - Validates               │DONE
 │  - Calls compute()         │
 └──────────────┬─────────────┘
                │ function call
@@ -24,10 +24,17 @@
 │  - Return structured data  │
 └────────────────────────────┘
  */
-let totalinput
-let tenderedinput
-let calcbutton 
-let output
+let totalinput = document.getElementById("amount-due")
+let tenderinput = document.getElementById("amount-received")
+let calcbutton = document.getElementById("calculate-change")
+calcbutton.addEventListener("click",inputHandler)
+let output = document.getElementById("coin-output")
+//UI
+//1.Populate all elements with proper text in DOM
+//2.Displays results inside of make change function as a new object inside of output element
+
+
+
 
 //Compute Layer
 function makeChange(total,tendered){
@@ -47,8 +54,19 @@ console.log(makeChange(40,100))
 
 // Event handler
 
-//1.Reads totalinput and tenderedinput, and then verifies the input with regex
-
-//2. Triggers makechange function and passes the data totalinput and tenderedinput into total and tendered as a integer
-
-//3. Spit result back into output
+//1.Reads totalinput and tenderedinput
+//2.Verifies inputs with regex
+//3. Triggers makechange function and passes the data totalinput and tenderedinput into total and tendered as a integer
+function inputHandler(){
+if (/^[0-9]+\.[0-9]{2}$/.test(totalinput.value) && /^[0-9]+\.[0-9]{2}$/.test(tenderinput.value))
+    {let resultOfMakeChange = makeChange(totalinput.value, tenderinput.value)
+let displayString = ""
+for (let key in resultOfMakeChange) {
+    displayString += key + ": " + resultOfMakeChange[key] + "\n"
+}
+output.textContent = displayString}
+else {let displayString = "Invalid Entry" 
+    output.textContent = displayString 
+}
+return 
+}
